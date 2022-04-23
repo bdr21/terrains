d3ba208dcbf5a702dc131ba5dacf5776f8a2e0ee
bdr21 <omarbdr1999@gmail.com>
2022-04-23 13:50:02 +0000

    après séance j2ee du 23/04.

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -10,11 +10,11 @@ public class Test {
     public static void main(String args[])
     {
         ClientDao cdi = new ClientDaoImpl();
-//        ArrayList<Client> listc = (ArrayList<Client>) cdi.getClients();
-//        for(Client c:listc) {
-//            System.out.println(c);
-//        }
-        Client c = new Client("5","koko","15667")
-        cdi.updateClient(c)
+        ArrayList<Client> listc = (ArrayList<Client>) cdi.getClients();
+        for(Client c:listc) {
+            System.out.println(c);
+        }
+//        Client c = new Client(5,"koko","15667");
+//        cdi.updateClient(c);
     }
 }
---
+++ src/main/webapp/addTerrain.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: DELL
+  Date: 20/04/2022
+  Time: 15:58
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+    <form action = "addTerrain.jsp" method = "POST">
+        First Name: <input type = "text" name = "first_name">
+        <br />
+        Last Name: <input type = "text" name = "last_name" />
+        <input type = "submit" value = "Submit" />
+    </form>
+</body>
+</html>
