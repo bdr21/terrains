e0414318f44d91acce20cb2c06aa877d0a039fd4
bdr21 <omarbdr1999@gmail.com>
2022-04-25 17:52:12 +0000

    commit test file

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -9,11 +9,13 @@ public class Test {
 
     public static void main(String args[])
     {
-        ClientDao cdi = new ClientDaoImpl();
-        ArrayList<Client> listc = (ArrayList<Client>) cdi.getClients();
-        for(Client c:listc) {
-            System.out.println(c);
-        }
+//        ClientDao cdi = new ClientDaoImpl();
+//        ArrayList<Client> listc = (ArrayList<Client>) cdi.getClients();
+//        for(Client c:listc) {
+//            System.out.println(c);
+//        }
+        String str = "delete";
+        System.out.println(str.matches("put|del"));
 //        Client c = new Client(5,"koko","15667");
 //        cdi.updateClient(c);
     }
